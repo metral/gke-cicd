fae222664e2a6570edb5040aa30ede9265c30df1
Mike Metral <1112768+metral@users.noreply.github.com>
2020-01-14 15:30:33 -0800

    init gh action

--- infra/index.ts
+++ infra/index.ts
@@ -17,7 +17,7 @@ const stackName = pulumi.getStack();
 
 // Create the GKE cluster developers ServiceAccount.
 const devsName = "devs";
-const devsAccountId = `k8s-${stackName}-${devsName}`;
+const devsAccountId = `k8s-${stackName.substring(0-7)}-${devsName}`;
 const devsIamServiceAccount = new gcp.serviceAccount.Account(devsName, {
     project: config.gcpProject,
     accountId: devsAccountId,
