b62cc71bf4a7fd236137ef85c668594da8094745
Mike Metral <1112768+metral@users.noreply.github.com>
2020-01-16 21:34:53 -0800

    app: cleanup

--- app/index.ts
+++ app/index.ts
@@ -3,18 +3,6 @@ import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 import * as app from "./app";
 import {config} from "./config";
-var fs = require("fs");
-
-/*
-// Read the local (KinD) cluster's KUBECONFIG.
-var kubeconfigData = fs.readFileSync(process.env.KUBECONFIG);
-const kubeconfig: pulumi.Output<any> = pulumi.output(kubeconfigData.toString());
-
-// Create a k8s provider for the local KinD cluster.
-const localProvider = new k8s.Provider("localProvider", {
-    kubeconfig: kubeconfig,
-});
-*/
 
 // Create a k8s provider for the remote GKE cluster.
 const gkeProvider = new k8s.Provider("gkeProvider", {
@@ -24,7 +12,6 @@ const gkeProvider = new k8s.Provider("gkeProvider", {
 
 // Create the application on each of the selected clusters.
 const instance = new app.DemoApp("demo", {
-    // provider: localProvider,
     provider: gkeProvider,
 });
 
