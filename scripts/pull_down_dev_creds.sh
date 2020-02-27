42a920dd494bb2b61638a364c94da10291790d2c
Mike Metral <1112768+metral@users.noreply.github.com>
2020-02-26 18:31:18 -0800

    add scripts dir

---
+++ scripts/pull_down_dev_creds.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# For Google, we need to authenticate with a service principal for
+# certain authentication operations.
+if [ ! -z "$GOOGLE_CREDENTIALS" ]; then
+    export GOOGLE_APPLICATION_CREDENTIALS="$(mktemp).json"
+    echo "$GOOGLE_CREDENTIALS" > $GOOGLE_APPLICATION_CREDENTIALS
+    gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+fi
+
+# Pull down Service Account Secret key for Developers to auth as a k8s dev.
+gsutil cp gs://dev-sa-secret/key.json /tmp/key.json && \
+	gcloud auth activate-service-account --key-file /tmp/key.json && \
+	gcloud auth configure-docker
